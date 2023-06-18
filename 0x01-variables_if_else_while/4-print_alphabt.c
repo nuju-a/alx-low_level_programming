1f5f4de75098b5ce9d84760a5c04b5c1701886af
nuju-a <learnnuju@gmail.com>
2023-06-18 03:06:42 +0100

    c variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: return 0
+  */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' && ch == 'q')
+		{
+			continue;
+		}
+	}
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
