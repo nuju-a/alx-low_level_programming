9fbb37ff80ee8f5642cc13b3395477d846aefc06
nuju-a <learnnuju@gmail.com>
2023-06-18 02:50:33 +0100

    c variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,9 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-
-	{
-		for (ch = 'A'; ch <= 'Z'; ch++)
-			putchar(ch);
-		putchar('\n');
-	}
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
 	return (0);
 }
