d6b3c48323e1aef013df913d0ece99b753500fd5
nuju-a <learnnuju@gmail.com>
2023-06-19 16:15:13 +0100

    c variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: return 0
+  */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 15; num++)
+	{
+		if (num == 'A' || num == 'F')
+		{
+			num = num  | 0x20;
+		putchar(num);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
