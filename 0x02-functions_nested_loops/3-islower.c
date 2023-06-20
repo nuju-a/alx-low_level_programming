73115604aa9d255f484b843f33bea5328c72cd13
nuju-a <learnnuju@gmail.com>
2023-06-21 00:57:19 +0100

    c functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,18 +4,20 @@
 
 /**
   *_islower -  Entry point
-  *'c': checks for lower case character
+  *
+  *c -  c takes on the value for lower case check is performed
+  *
   *Return: return 0
   */
 int _islower(int c)
 {
 	if (c != 0)
 	{
-		_putchar(1);
+		_putchar(c + '0');
 	}
 	else
 	{
-		_putchar(0);
+		_putchar(c + '0');
 	}
 	return (0);
 }
