a17f3f79547b4b784615c49ef3f617571533ec5d
nuju-a <learnnuju@gmail.com>
2023-06-20 02:21:12 +0100

    c variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Return: return 0
+  */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 56; a++)
+		for (b = a + 1; b <= 57; b++)
+		{
+			putchar(a);
+			putchar(b);
+			if (a < 56 || b < 57)
+			{
+				putchar(' ');
+				putchar(',');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
