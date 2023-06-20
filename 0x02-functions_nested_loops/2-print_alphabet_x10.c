be162527240bd973501b2731eb97968b076b8d10
nuju-a <learnnuju@gmail.com>
2023-06-20 22:53:49 +0100

    c functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 - Entry point
+  *
+  *Description: will print a-z 10times
+  *Return: return 0
+  */
+void print_alphabet_x10(void)
+{
+	int b = 0;
+
+	while (b < 11)
+	{
+		int a = 97;
+		while (a < 123)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar(b);
+		b++;
+	}
+}
+
