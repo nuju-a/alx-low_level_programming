4ad998eb9f84c7b2949b5162213cbed9becb075f
nuju-a <learnnuju@gmail.com>
2023-06-21 14:17:13 +0100

    c functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,16 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
-		_putchar('+');
+		return (+1);
 	}
 	else if
 		(n < 0)
 		{
 			return (-1);
-			_putchar('-');
 		}
 	else
 	{
-			return (0);
+			return (00);
 			_putchar(0);
 	}
 }
