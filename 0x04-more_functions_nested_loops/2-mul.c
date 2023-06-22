4ba0c2f34cd7ed1e4c06328d5a5c3a48b0916b41
nuju-a <learnnuju@gmail.com>
2023-06-22 22:58:06 +0100

    multiplies integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *mul - Entry point
+  *Description - function multiplies two integers
+  *@a: First argument for mul operation
+  *@b: Second argument for mul operation
+  *@result: argument that stores answer
+  *Return: return 0
+  */
+int mul(int a, int b)
+{
+	int result;
+
+	_putchar("%d", result);
+	return (0);
+}
