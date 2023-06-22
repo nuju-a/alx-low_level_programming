287fbb2a731a2664b67f385e59011cf03e737a88
nuju-a <learnnuju@gmail.com>
2023-06-22 15:05:57 +0100

    + or - test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *positive_or_negative - tests function that prints if integer is positive or negative
+  *@i: integer for which test is run
+  *Return: 0
+  */
+int positive_or_negative(int i)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+The project is to learn how we can debug c functions using different debug means eg print debug and also learning how to expliciting expalin your code lines to fish error out via rubber ducking
