6e4aeb5e95da35068889cf9204cc0d991d81e829
nuju-a <learnnuju@gmail.com>
2023-06-22 23:48:41 +0100

    multiplies integer

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
+
 /**
   *mul - Entry point
   *Description - function multiplies two integers
   *@a: First argument for mul operation
   *@b: Second argument for mul operation
-  *@result: argument that stores answer
   *Return: return 0
   */
 int mul(int a, int b)
 {
 	int result;
 
-	_putchar("%d", result);
+	for (a = 0; a <= 9; a++)
+		for (b = 0; b <= 9; b++)
+			result = a * b;
+	_putchar(result + '0');
 	return (0);
 }
