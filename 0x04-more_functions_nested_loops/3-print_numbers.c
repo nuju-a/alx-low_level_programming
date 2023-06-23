8e66dd7e03ec43441f742fc2aa7b44da2913429b
nuju-a <learnnuju@gmail.com>
2023-06-23 12:22:14 +0100

    prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *print_number - Entry point
+  *
+  *Return: 0
+  */
+void print_numbers(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
