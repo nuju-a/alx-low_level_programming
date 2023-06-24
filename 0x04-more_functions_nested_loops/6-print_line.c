8a9813a93cdce569797004090ead4ee717a20055
nuju-a <learnnuju@gmail.com>
2023-06-24 05:26:18 +0100

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *print_line - Entry point
+  *
+  *Return: 0
+  */
+void print_line(int n)
+{
+
+	if (n == 0 || n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(95);
+		_putchar('\n');
+	}
+}
