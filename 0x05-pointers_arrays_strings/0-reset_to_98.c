c08a0510f8fa7048ad58dcbabbba5fc684c67c70
nuju-a <learnnuju@gmail.com>
2023-06-26 15:50:43 +0100

    c pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  *reset_to_98 - Entry point
+  *@n: argument that points to the variable type
+  *Return: 0
+  */
+void retest_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory focus on the use of pointers, arrays and strings in C, its uses, manipulation and how it helps for better understanding of the language.
