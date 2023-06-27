2b6404113ce725beb99376e156114320285395a8
nuju-a <learnnuju@gmail.com>
2023-06-27 04:54:11 +0100

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  *rev_string - Entry point
+  *@s: The character checks the output
+  *Return: 0
+  */
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i = len - 1;
+
+
+	while (i >= len)
+
+	{
+		len--;
+	}
+
+}
