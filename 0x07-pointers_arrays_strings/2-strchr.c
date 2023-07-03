e18b2e26e24deb98289c7fe3c70b46e5e49a02e8
nuju-a <learnnuju@gmail.com>
2023-07-03 21:43:52 +0100

    strcpy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+  *_strchr - Entry point
+  *Description - function locates a character in a string
+  *@s: the string in which charcter is searched for
+  *@c: the character to be searched
+  *Return: return a pointer to first occurence of c
+  */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
