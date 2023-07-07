d9298111d1f74b12b5bc7af6bc7418c6c88dcd19
nuju-a <learnnuju@gmail.com>
2023-07-08 00:30:03 +0100

    recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *is_palindrome - Entry point
+ *@s: pointer to character to check
+ *Return: returns 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	int p = strlen(s);
+
+	if (s[0] == '\0' || s[1] == '\0')
+	{
+		return (1);
+	}
+	if (s[0] == s(p - 1)
+			{
+	retirn
+	}
+}
