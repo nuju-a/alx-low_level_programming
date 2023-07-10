ca8a08d20a61834b3548caeca652c76e8d854edc
nuju-a <learnnuju@gmail.com>
2023-07-10 01:08:11 +0100

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *main - Entry point
+  *@argc: agrument count
+  *@agrv: agrument array
+  *
+  *Description - the string literal
+  *Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory is focused on how c main changes from usual void to taking on argument count and argument vector(array) to further simply process.
