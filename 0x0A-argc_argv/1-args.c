c82ecd2f5182ecfee13bdfe6160e1e908adb6f07
nuju-a <learnnuju@gmail.com>
2023-07-10 02:26:39 +0100

    count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *main - Entry point
+  *@argc: argument count
+  *@argv: argument array
+  *
+  *Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
