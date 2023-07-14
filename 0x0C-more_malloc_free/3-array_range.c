563d0ef985259fc9278387a00757622337abf96a
nuju-a <learnnuju@gmail.com>
2023-07-14 03:32:16 +0100

    range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	size = max - (min + 1);
+	size = max - min + 1;
 	arr = malloc(size * sizeof(int));
 
 	if (arr == NULL)
@@ -25,7 +25,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 		arr[i] = min++;
 
 	return (arr);
