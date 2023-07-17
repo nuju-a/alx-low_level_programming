d5353c03241f9d040dfec2bb62badcab7ed6f45a
nuju-a <learnnuju@gmail.com>
2023-07-17 22:01:32 +0100

    file

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define SOURCEFILE __FILE__
+
+/**
+  * main - Entry point
+  *Return: 0
+  */
+
+int main(void)
+{
+do {
+	printf("%s\n", SOURCEFILE);
+} while (0);
+
+return (0);
+}
