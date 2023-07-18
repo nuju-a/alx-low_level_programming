e8608b69b12fe4f13c3556b7369bec4f54404470
nuju-a <learnnuju@gmail.com>
2023-07-19 00:42:48 +0100

    struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+  *print_dog - defines a user structure
+  *@d: structure argument
+  *
+  *Return: pointer to dog
+  */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else if (d == NULL)
+	{
+		printf("nil\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+
+}
