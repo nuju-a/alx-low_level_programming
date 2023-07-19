107c939220e9f05cbbfa74070ad1b5b469bd6707
nuju-a <learnnuju@gmail.com>
2023-07-19 23:07:21 +0100

    printname

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
   *print_name - Entry point for function name
@@ -9,5 +10,6 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL)
+		f(name);
 }
