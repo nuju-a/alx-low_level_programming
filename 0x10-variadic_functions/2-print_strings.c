d5f905eaf67965445469285bfcba1e4373a900b7
nuju-a <learnnuju@gmail.com>
2023-07-20 23:35:59 +0100

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,27 +14,30 @@ void print_strings(const char *seperator, const unsigned int n, ...)
 	const char *arg;
 	va_list string;
 
-	if (n == 0)
+	/*if (n == 0)
 	{
 		printf("(nil)");
 		return;
-	}
+	}*/
 
 	va_start(string, n);
 	for (i = 0; i < n; i++)
 	{
 		arg = va_arg(string, const char*);
 
-		if (arg != NULL || arg[0] != '\0')
+		if (n == 0)
 		{
-			printf("%s", arg);
+			printf("(nil)");
+			return;
+		}
+		/*if (arg != NULL || arg[0] != '\0')*/
+		printf("%s", arg);
 			count++;
 
 			if (seperator != NULL && count < n)
 			{
 				printf("%s", seperator);
 			}
-		}
 	}
 	printf("\n");
 	va_end(string);
