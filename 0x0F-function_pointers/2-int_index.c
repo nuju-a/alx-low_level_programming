909f851688f8af3c795d90ed2656d368782278d6
nuju-a <learnnuju@gmail.com>
2023-07-20 02:02:05 +0100

    array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,17 +11,17 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-		{
-			return (i);
-		}
-	}
 	if (size <= 0)
-	{
 		return (-1);
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
 	}
 	return (-1);
 }
