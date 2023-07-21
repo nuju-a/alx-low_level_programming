e3d02f6d7192ba4f85b12d6ddb97bde0d7c96a95
nuju-a <learnnuju@gmail.com>
2023-07-21 03:25:38 +0100

    printsall

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,46 +9,44 @@
   */
 void print_all(const char * const format, ...)
 {
-    va_list print;
-    const char *p = format;
-    const char *s = ", ";
-    char *str;
+	va_list print;
+	const char *p = format;
+	const char *s = ", ";
+	char *str;
 
     /*handle null format string*/
-    if (!format)
-    {
-        printf("\n");
-        return;
-    }
+	if (!format)
+	{
+		printf("\n");
+		return;
+	}
 
     /*iterate over format string*/
-    va_start(print, format);
-    while (*p)
-    {
-        /* check if not first argument, print separator*/
-        printf("%s", (p != format) ? s : "");
+	va_start(print, format);
+	while (*p)
+	{
+		printf("%s", (p != format) ? s : "");
 
-        /* switch statement for format specifiers*/
-	switch (*p)
-        {
-            case 'c':
-                printf("%c", va_arg(print, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(print, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(print, double));
-                break;
-            case 's':
-                str = va_arg(print, char *);
-                printf("%s", str ? str : "(nil)");
-                break;
-            default:
-                break;
-        }
-        p++;
-    }
-    printf("\n");
-    va_end(print);
+		switch (*p)
+		{
+			case 'c':
+				printf("%c", va_arg(print, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(print, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(print, double));
+				break;
+			case 's':
+				str = va_arg(print, char *);
+				printf("%s", str ? str : "(nil)");
+				break;
+			default:
+				break;
+		}
+		p++;
+	}
+	printf("\n");
+	va_end(print);
 }
