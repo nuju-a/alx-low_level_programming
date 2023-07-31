e9f6090c0e358b4adacd29be42d8ea7ade0a697b
Nuju Azenabor <nujuazenabor@gmail.com>
2023-07-31 17:44:03 +0100

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,37 +1,42 @@
 #include "main.h"
 
 /**
-  *string_nconcat - Entry point
-  *@s1: first parameter
-  *@s2: second parameter
+  *string_nconcat - Concate function that appends src str to dest
+  *@s1: The destination string
+  *@s2: The source string
   *
-  *@n: n byte of the pointer
-  *Return: 0
+  *Return: returns a pointer to strncat
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	size_t dest = strlen(s1);
+	size_t src = strlen(s2);
+	size_t i;
 	char *result;
-	//size_t len1;
-	//size_t len2;
-	unsigned int size = n, t_len;
 
 	if (s1 == NULL || s2 == NULL)
 	{
 		return ("");
 	}
-	result = malloc((size + 1) * sizeof(char));
+	if (n >= src)
+	{
+		n = src;
+	}
+	result = malloc(sizeof(char) * (dest + n + 1));
 
 	if (result == NULL)
 	{
 		return (NULL);
 	}
-	size = 0;
-	for (t_len = 0; s1[t_len]; t_len++)
-		result[size++] = s1[t_len];
-	for (t_len = 0; s2[t_len] && t_len < n; t_len++)
-		result[size++] = s2[t_len];
-
-	result[size] = '\0';
+	for (i = 0; i < dest; i++)
+	{
+		result[i] = s1[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		result[dest + i] = s2[i];
+	}
+	result[dest + n] = '\0';
 
 	return (result);
 }
