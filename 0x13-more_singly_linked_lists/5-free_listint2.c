2688a7959b2cc03934c57b1a44d5a026d5685148
Nuju Azenabor <nujuazenabor@gmail.com>
2023-08-01 21:24:32 +0100

    correctfree

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,17 +11,17 @@ void free_listint2(listint_t **head)
 	listint_t *temp, *last;
 
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		return;
 	}
-	last = head;
+	last = *head;
 
-	while (last != NULL)
+	while (last == NULL)
 	{
 		temp = last;
 		last = last->next;
 		free(temp);
 		}
-	head = NULL;
+	*head = NULL;
 }
