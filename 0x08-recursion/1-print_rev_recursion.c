7bef54d1f6c02a4ce027ee0f8a4da66338d217e7
Nuju Azenabor <nujuazenabor@gmail.com>
2023-09-28 22:43:10 +0100

    prints string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_print_rev_recursion - funtion that prints string in reverse
+  *@s: parameter that returns a character
+  *
+  *Return: 0
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
