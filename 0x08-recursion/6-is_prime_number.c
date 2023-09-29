ad879b0b1d9645d4cc5f9022052b86d84d7cd854
Nuju Azenabor <nujuazenabor@gmail.com>
2023-09-29 02:09:40 +0100

    returns 1 if prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *is_prime_number - Entry point
+ *@n: input integer
+ *Return: return 1 or 0.
+ */
+int is_prime_number(int n)
+{
+	int t = n % 10;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (t % 2 == 0 || t % 5 == 0)
+	{
+		return (0);
+	}
+	if (n / n == 1 || n / 1 == n)
+	{
+		return (1);
+	}
+	return (is_prime_number(n));
+}
