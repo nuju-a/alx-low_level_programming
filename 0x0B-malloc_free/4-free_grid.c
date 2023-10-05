9d097b5eaf609f5cc4d0acf79ab5b6b3fd78cb42
Nuju Azenabor <nujuazenabor@gmail.com>
2023-10-05 23:58:49 +0100

    freegrid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,15 +11,8 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL)
-	{
-		return;
-	}
+	for (i = 0; i < height; i++)
+		free(grid[i]);
 
-    for (i = 0; i < height; i++)
-    {
-	    free(grid[i]);
-    }
-
-    free(grid);
+	free(grid);
 }
