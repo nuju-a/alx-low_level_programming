08ad64317d4e09491c65f24d2348dc5762af2a52
Nuju Azenabor <nujuazenabor@gmail.com>
2023-10-10 20:37:08 +0100

    modified the if statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,22 +7,19 @@
   */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if (d->name == NULL)
-		printf("Name: nil");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age < 0)
-		printf("Age: nil");
-	else
-		printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner: nil");
-	else
-		printf("Owner: %s\n", d->owner);
-	}
-	else
+	if (d == NULL)
 		return;
 
+	if (d->name == NULL || d->owner == NULL || d->age < 0)
+	{
+		printf("nil");
+		return;
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+
+	}
 }
