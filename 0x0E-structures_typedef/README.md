ccb90e6b1a5013fa18796b546da9c9ce5f6c24dc
Nuju Azenabor <nujuazenabor@gmail.com>
2023-10-10 17:20:29 +0100

    stuct created

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+Directory focuses on structures and typedef. structure are user-defined data-type that combines different kinds of data items.
+typedef are used to give type new name.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef "MAIN_H"
+#define "MAIN_H"
+
+/*standard lib*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/*function prototype*/
+/**
+  *struct dog - User defined
+  *@name: Gives the name of the struct dog
+  *@age: Gives the age of the struct dog
+  *@owner: Tells the owner of struct dog
+  *
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*MAIN_H*/
