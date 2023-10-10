c9cdf9c166201f77fa598dc0d0d5b37400b70925
Nuju Azenabor <nujuazenabor@gmail.com>
2023-10-10 19:13:39 +0100

    modified printstruct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,10 @@ void print_dog(struct dog *d)
 		printf("Name: nil");
 	else
 		printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	if (d->age < 0)
+		printf("Age: nil");
+	else
+		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: nil");
 	else
