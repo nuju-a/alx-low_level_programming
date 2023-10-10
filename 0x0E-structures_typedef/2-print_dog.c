62e186e3781ce9e1f5a714c2f4157ad6d00f73ca
Nuju Azenabor <nujuazenabor@gmail.com>
2023-10-10 20:53:45 +0100

    rverted to if statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,25 +1,24 @@
 #include "dog.h"
 
 /**
-  *print_dog - Function prints a struct dog
-  *@d: struct initialization
-  *
-  */
+ *print_dog - Function prints a struct dog
+ *@d: struct initialization
+ *
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-
-	if (d->name == NULL || d->owner == NULL || d->age < 0)
-	{
-		printf("nil");
-		return;
-	}
+	if (d->name == NULL)
+		printf("Name: nil");
 	else
-	{
 		printf("Name: %s\n", d->name);
+	if (d->age < 0)
+		printf("Age: nil");
+	else
 		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: nil");
+	else
 		printf("Owner: %s\n", d->owner);
-
-	}
 }
