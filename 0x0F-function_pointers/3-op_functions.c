2d28fa784535babffc3dad1cf436288daf4252fb
Nuju Azenabor <nujuazenabor@gmail.com>
2023-10-12 03:24:39 +0100

    simpleop

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+  *op_add - Function that adds two integer
+  *@a: first parameter
+  *@b: second parameter
+  *
+  *Return: 0
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  *op_sub - Function that minus two integer
+  *@a: first parameter
+  *@b: second parameter
+  *
+  *Return: 0
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  *op_mul - Function that multiplues two integer
+  *@a: first parameter
+  *@b: second parameter
+  *
+  *Return: 0
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  *op_div - Function that divides two integer
+  *@a: first parameter
+  *@b: second parameter
+  *
+  *Return: 0
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  *op_mod - Function that giver the remainder of two integers
+  *@a: first parameter
+  *@b: second parameter
+  *
+  *Return: 0
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
