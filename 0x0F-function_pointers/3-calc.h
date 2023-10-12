c1054797de6c7c18843ef97738995fda9c9bddfd
Nuju Azenabor <nujuazenabor@gmail.com>
2023-10-12 03:31:38 +0100

    modified

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,23 +1,24 @@
 #ifndef FUNCOP_H
 #define FUNC0P_H
-
+/*standard lib*/
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-
+/*struct definfition*/
 /**
  * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
-
+/*function prototype*/
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
