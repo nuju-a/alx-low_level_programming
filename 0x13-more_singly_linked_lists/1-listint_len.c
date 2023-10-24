a709ab6b86092578353fe527c2b2671668511554
Nuju Azenabor <nujuazenabor@gmail.com>
2023-10-24 14:21:23 +0100

    singy len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  *listint_len - function return the lenght of list
+  *@h: pointer to the listint_t const
+  *
+  *Return: number of elements
+  */
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return(len);
+}
+
