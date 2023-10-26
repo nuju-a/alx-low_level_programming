9e2d90fba2409e5708b68d673b850730e9b9a557
Nuju Azenabor <nujuazenabor@gmail.com>
2023-10-26 22:30:51 +0100

    bitwise

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  *binary_to_uint - function that converts binary to unsigned int
+  *@b: const char that points to current conversion unit
+  *
+  *Return: converted number of 0
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	int len = strlen(b);
+	int i;
+	char c;
+	unsigned int sum = 0, mult = 1;
+
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; i < len; i++)
+	{
+		c = b[i];
+		if (c != '0' && c != '1')
+			return (0);
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (b[len - 1 - i] == '1')
+			sum += (b[len - 1 - i] - '0') * mult;
+
+		mult *= 2;
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,7 @@
+This directory will focus on bit wise operation that is the manipulation of the number base system and the performing of bitwise operation
+& = bitwise AND
+| = bitwise OR
+~ = bitwise NOT
+>> = bitwise right shift
+<< = bitwise left shift
+^ = bitwise exclusive OR
